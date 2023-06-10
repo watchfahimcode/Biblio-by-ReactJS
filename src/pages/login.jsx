887c9e7bc1b60c5861e00c2034e497df4d515e17
Fahim Rahman <0fahim.r@gmail.com>
2023-06-10 14:11:31 +0600

    Login Design

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,7 +1,54 @@
+import React, { useState } from 'react';
+import { Container, Form, Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Link } from 'react-router-dom';
+
 export default function Home() {
-    return (
-      <div>
-        <h1>Login</h1>
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = () => {
+    // Perform login functionality using the username and password
+  };
+
+  return (
+    <Container className="d-flex justify-content-center align-items-center vh-100">
+      <div className="login-form shadow p-4">
+      <h1>Login</h1>
+        <Form>
+          <Form.Group>
+            <Form.Label>Username:</Form.Label>
+            <Form.Control
+              type="text"
+              value={username}
+              onChange={handleUsernameChange}
+            />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Password:</Form.Label>
+            <Form.Control
+              type="password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+          </Form.Group>
+          <Button variant="primary" onClick={handleLogin}>
+            Login
+          </Button>
+        </Form>
+        <div className="mt-3 text-center">
+          Don't have an account? <Link to="/register">Sign up</Link>
+        </div>
       </div>
-    );
-  }
+    </Container>
+  );
+}
+
